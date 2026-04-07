9ecc86fdb097b01b76f1b068cd5bf2b265ad316e
Giuliano Lemes Pereira <giuice@gmail.com>
2026-04-07 18:08:40 -0300

    feat(02-03): export shared libs and copy templates
    - add the shared library barrel export
    - copy template sources into dist during postbuild

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export { deepMerge, deduplicateHookArray, mergeMarkerSection } from "./merge.js";
+export { scaffoldProject } from "./scaffold.js";
+export type { ScaffoldOptions } from "./scaffold.js";
+export { detectTools } from "./detect.js";
+export { formatReport } from "./reporter.js";
+export type { ReportAction, ReportEntry } from "./reporter.js";
